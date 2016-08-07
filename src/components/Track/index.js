5f7f1b2c90fbfe44b6f65d9a09ceec776c63e5ba
Robin Wieruch <wrobin@gmx.net>
2016-08-07 22:47:45 +0200

    refactor(Track): use observer

--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -1,37 +1,58 @@
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React from 'react';
+import { observer } from 'mobx-react';
 import * as actions from '../../actions/index';
 import { TrackPlaylist } from './playlist';
 import { TrackPreview } from './preview';
 import { TrackStream } from './stream';
+import playerStore from '../../stores/playerStore';
+import entityStore from '../../stores/entityStore';
+import userStore from '../../stores/userStore';
+import filterStore from '../../stores/filterStore';
+import sortStore from '../../stores/sortStore';
 
-function mapStateToProps(state, props) {
-  const { idx, activity } = props;
+// const TrackPlaylistContainer = observer(({ idx, activity }) => {
+//   return (
+//     <TrackPlaylist
+//       idx={idx}
+//       activity={activity}
+//       typeReposts={userStore.typeReposts}
+//       typeTracks={userStore.typeTracks}
+//       userEntities={entityStore.users}
+//       isPlaying={playerStore.isPlaying}
+//       activeTrackId={playerStore.activeTrackId}
+//       activeSortType={sortStore.sortType}
+//       activeDurationFilterType={filterStore.durationFilterType}
+//       onActivateTrack={actions.activateTrack}
+//       onAddTrackToPlaylist={actions.addTrackToPlaylist}
+//       onRemoveTrackFromPlaylist={actions.removeTrackFromPlaylist}
+//     />
+//   );
+// });
 
-  return {
-    idx,
-    activity,
-    typeReposts: state.user.typeReposts,
-    typeTracks: state.user.typeTracks,
-    userEntities: state.entities.users,
-    isPlaying: state.player.isPlaying,
-    activeTrackId: state.player.activeTrackId,
-    activeSortType: state.sort.sortType,
-    activeDurationFilterType: state.filter.durationFilterType,
-  };
+function TrackContainer(component) {
+  return observer(({ idx, activity }) => {
+    return (
+      <component
+        idx={idx}
+        activity={activity}
+        typeReposts={userStore.typeReposts}
+        typeTracks={userStore.typeTracks}
+        userEntities={entityStore.users}
+        isPlaying={playerStore.isPlaying}
+        activeTrackId={playerStore.activeTrackId}
+        activeSortType={sortStore.sortType}
+        activeDurationFilterType={filterStore.durationFilterType}
+        onActivateTrack={actions.activateTrack}
+        onAddTrackToPlaylist={actions.addTrackToPlaylist}
+        onRemoveTrackFromPlaylist={actions.removeTrackFromPlaylist}
+      />
+    );
+  });
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    onActivateTrack: bindActionCreators(actions.activateTrack, dispatch),
-    onAddTrackToPlaylist: bindActionCreators(actions.addTrackToPlaylist, dispatch),
-    onRemoveTrackFromPlaylist: bindActionCreators(actions.removeTrackFromPlaylist, dispatch),
-  };
-}
-
-const TrackPlaylistContainer = connect(mapStateToProps, mapDispatchToProps)(TrackPlaylist);
-const TrackPreviewContainer = connect(mapStateToProps, mapDispatchToProps)(TrackPreview);
-const TrackStreamContainer = connect(mapStateToProps, mapDispatchToProps)(TrackStream);
+const TrackPreviewContainer = TrackContainer(TrackPreview);
+const TrackStreamContainer = TrackContainer(TrackStream);
+const TrackPlaylistContainer = TrackContainer(TrackPlaylist);
 
 export {
   TrackPlaylistContainer,
