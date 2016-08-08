4acbfac1af7e0fd9ee6392799f524aef269f876b
Robin Wieruch <wrobin@gmx.net>
2016-08-08 19:57:12 +0200

    refactor(stores): remove configureStore

--- src/index.js
+++ src/index.js
@@ -3,34 +3,24 @@ import SC from 'soundcloud';
 /*eslint-enable */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { Provider } from 'react-redux';
-import configureStore from './stores/configureStore';
+import { Router, Route, IndexRoute } from 'react-router';
 import { Dashboard } from './components/Dashboard';
 import Browse from './components/Browse';
-import { Fave } from './components/Fave';
 import Callback from './components/Callback';
 import App from './components/App';
-import { browse, dashboard, fave, callback } from './constants/pathnames';
+import { browse, dashboard, callback } from './constants/pathnames';
 
 require('../styles/index.scss');
 
-const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Browse} />
-        <Route path={callback} component={Callback} />
-        <Route path={dashboard} component={Dashboard} />
-        <Route path={browse} component={Browse} />
-        <Route path={fave} component={Fave} />
-        <Route path="*" component={Browse} />
-      </Route>
-    </Router>
-  </Provider>,
+  <Router history={history}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Browse} />
+      <Route path={callback} component={Callback} />
+      <Route path={dashboard} component={Dashboard} />
+      <Route path={browse} component={Browse} />
+      <Route path="*" component={Browse} />
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
