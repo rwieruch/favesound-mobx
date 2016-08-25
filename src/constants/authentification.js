bfb144ecb4ab56da23a2cdf1f7d852b704477c92
Robert Klep <robert@klep.name>
2016-08-25 20:03:15 +0200

    Fixed react-addons-test-utils version, added option to read SoundCloud client ID from environment

--- src/constants/authentification.js
+++ src/constants/authentification.js
@@ -4,7 +4,7 @@ export const REDIRECT_URI = isDev ?
   `${window.location.protocol}//${window.location.host}/callback` :
   'http://www.favesound.de/callback';
 
-export const CLIENT_ID = isDev ?
+export const CLIENT_ID = process.env.SOUNDCLOUD_CLIENT_ID || isDev ?
     'a281614d7f34dc30b665dfcaa3ed7505' :
     '1512fb9cbe8228095fe92c6503e3a071';
 
