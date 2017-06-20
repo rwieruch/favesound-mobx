a3c9b15b6108267e63b24230fdfd98b0ac4369ab
Ye <field0917@gmail.com>
2017-06-20 13:37:30 -0700

    Upgraded react router to v4

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { DEFAULT_GENRE } from '../../constants/genre';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Browse from '../../components/Browse';
+import Callback from '../../components/Callback';
+import Dashboard from '../../components/Dashboard';
 import Header from '../../components/Header';
 import Player from '../../components/Player';
 import Playlist from '../../components/Playlist';
+import { browse, dashboard, callback } from '../../constants/pathnames';
 
 export default class App extends React.Component {
 
   render() {
-    const { location, children } = this.props;
-    const { pathname, query } = location;
-    const genre = query.genre || DEFAULT_GENRE;
-
     return (
       <div>
-        <Header genre={genre} pathname={pathname} />
-          {children}
+        <Header />
+        <Switch>
+          <Route exact path={browse} component={Browse} />
+          <Route exact path={dashboard} component={Dashboard} />
+          <Route exact path={callback} component={Callback} />
+          <Redirect to={browse} />
+        </Switch>
         <Playlist />
         <Player />
       </div>
